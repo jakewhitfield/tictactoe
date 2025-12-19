d4ad3bd3d67f1dea6ffd5651e3e8b85ef3b86ba1
Jake Whitfield <jake.s.whitfield@gmail.com>
2025-12-19 11:48:32 +1100

    Added Win/Draw States

--- board.py
+++ board.py
@@ -2,6 +2,7 @@ class Board:
     def __init__(self):
         self.board = [[" " for x in range(3)] for y in range(3)]
         self.empty_line = "   "
+        self.turns_completed = 0
 
     def draw_board(self):
         print("-------------")
@@ -11,9 +12,42 @@ class Board:
             print("|" + self.empty_line + "|" + self.empty_line + "|" + self.empty_line + "|")
             print("-------------")
 
-    def add_to_board(self, x_cord, y_cord, player):
-        if self.board[x_cord][y_cord] == " ":
-            self.board[x_cord][y_cord] = player
+    def add_to_board(self, x_coord, y_coord, player):
+        if self.board[x_coord][y_coord] == " ":
+            self.board[x_coord][y_coord] = player
+            self.turns_completed += 1
             return True
         else:
             return False
+
+    def check_game_end(self):
+        if self.board[0][0] != " ":
+            if self.board[0][0] == self.board[0][1] and self.board[0][0] == self.board[0][2]:
+                return self.board[0][0] + " has won the game!"
+            elif self.board[0][0] == self.board[1][0] and self.board[0][0] == self.board[2][0]:
+                return self.board[0][0] + " has won the game!"
+            elif self.board[0][0] == self.board[1][1] and self.board[0][0] == self.board[2][2]:
+                return self.board[0][0] + " has won the game!"
+
+        if self.board[0][1] != " ":
+            if self.board[0][1] == self.board[1][1] and self.board[0][1] == self.board[2][1]:
+                return self.board[0][1] + " has won the game!"
+
+        if self.board[0][2] != " ":
+            if self.board[0][2] == self.board[1][2] and self.board[0][2] == self.board[2][2]:
+                return self.board[0][2] + " has won the game!"
+
+        if self.board[1][0] != " ":
+            if self.board[1][0] == self.board[1][1] and self.board[1][0] == self.board[1][2]:
+                return self.board[1][0] + " has won the game!"
+
+        if self.board[2][0] != " ":
+            if self.board[2][0] == self.board[2][1] and self.board[2][0] == self.board[2][2]:
+                return self.board[2][0] + " has won the game!"
+            elif self.board[2][0] == self.board[1][1] and self.board[2][0] == self.board[0][2]:
+                return self.board[2][0] + " has won the game!"
+
+        if self.turns_completed == 9:
+            return "The game has ended in a draw!"
+
+        return ""
--- main.py
+++ main.py
@@ -1,5 +1,42 @@
 from board import Board
 
 board = Board()
+game_running = True
 
-print(board.draw_board())
+def player_turn(player_symbol):
+    board.draw_board()
+    print("It is now the '" + player_symbol + "' player's turn")
+
+    invalid_turn = True
+    while invalid_turn:
+        x_coord_not_valid = True
+        while x_coord_not_valid:
+            x = int(input("Enter the row number (0-2): "))
+            if x < 0 or x > 2:
+                print("Please enter a valid column number")
+            else:
+                x_coord_not_valid = False
+        y_coord_not_valid = True
+        while y_coord_not_valid:
+            y = int(input("Enter the column number (0-2): "))
+            if y < 0 or y > 2:
+                print("Please enter a valid row number")
+            else:
+                y_coord_not_valid = False
+
+        if board.add_to_board(x_coord=x, y_coord=y, player=player_symbol):
+            invalid_turn = False
+            game_message = board.check_game_end()
+            if game_message != "":
+                board.draw_board()
+                print(game_message)
+                return False
+        else:
+            print("That space is full, please choose another space")
+
+    return True
+
+while game_running:
+    game_running = player_turn("O")
+    if game_running:
+        game_running = player_turn("X")
